da11a5ffd6b794b49ce86f5dd016caf8b899f314
Happy_Ta <will231897@gmail.com>
2024-08-07 19:54:36 -0400

    Initial commit

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+using Verse;
+
+namespace SimplePathfinding {
+
+    [StaticConstructorOnStartup]
+    public static class HelloWorld {
+        static HelloWorld() {
+            Log.Message("Hello World!");
+        }
+    }
+}
